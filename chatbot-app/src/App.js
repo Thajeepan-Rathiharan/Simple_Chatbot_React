5e725163f62c902b6ce36460ca2c7ef9c78cb05c
Thajeepan-Rathiharan <thajee2002@gmail.com>
2024-03-18 15:46:49 +0000

    Updated app.js

--- chatbot-app/src/App.js
+++ chatbot-app/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'What is your name?',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            user: true,
+            trigger: '3',
+          },
+          {
+            id: '3',
+            message: 'Hi {previousValue}, nice to meet you!',
+            trigger: '4',
+          },
+          {
+            id: '4',
+            message: 'What number I am thinking?',
+            trigger: '5',
+          },
+          {
+            id: '5',
+            options: [
+              { value: 1, label: 'Number 1', trigger: '7' },
+              { value: 2, label: 'Number 2', trigger: '6' },
+              { value: 3, label: 'Number 3', trigger: '6' },
+            ],
+          },
+          {
+            id: '6',
+            message: 'Wrong answer, try again.',
+            trigger: '5',
+          },
+          {
+            id: '7',
+            message: 'Awesome! You are a telepath!',
+            end: true,
+          },
+        ]}
+        floating={true}
+      />
   );
 }
 
